7d14848c831f173b1a9485fe4b3c25236036721f
Dolu Obatusin <mobatusi@gmail.com>
2024-12-21 13:14:36 -0500

    complete lead qualification crew, agents and tasks

--- helper.py
+++ helper.py
@@ -11,3 +11,8 @@ def get_openai_api_key():
     load_env()
     openai_api_key = os.getenv("OPENAI_API_KEY")
     return openai_api_key
+
+def get_serper_api_key():
+    load_env()
+    serper_api_key = os.getenv("SERPER_API_KEY")
+    return serper_api_key
